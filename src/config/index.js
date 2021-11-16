25b64058e3d4327f9c153ab65cb5337050434da8
windxxcom <ez@windxx.com>
2021-11-16 15:09:07 +0800

    key

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
   type: '050000', //类别
   distance: 2000, //搜寻半径 取值范围:0-50000。
-  key: '0e1a0782c25c1b4045ffc0eec3789fcf', //高德地图私钥
+  key: '865f8f549c1ccc978b4ed43fe7ad3ef2', //高德地图私钥
   pageSize: 50, //每页数据条数
   sortrule: 'weight', // 排序规则 weight | distance
   extensions: 'all', //返回结果  base返回基本地址信息；取值为all返回地址信息、附近POI、道路以及道路交叉口信息。
